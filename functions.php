5fd4b7d71d4b2039b6741c5b3a2c0d92003652ca
kobenland <obenland@gmx.de>
2012-04-09 23:14:29 +0200

    Merge master

--- functions.php
+++ functions.php
@@ -8,11 +8,6 @@
  */
 
 
-if ( ! isset( $content_width ) ) {
-	$content_width = 770;
-}
-
-
 if ( ! function_exists( 'the_bootstrap_setup' ) ):
 /**
  * Sets up theme defaults and registers support for various WordPress features.
@@ -23,11 +18,17 @@ if ( ! function_exists( 'the_bootstrap_setup' ) ):
  * @return	void
  */
 function the_bootstrap_setup() {
+	global $content_width;
+	
+	if ( ! isset( $content_width ) ) {
+		$content_width = 770;
+	}
 	
 	load_theme_textdomain( 'the-bootstrap', get_template_directory() . '/lang' );
 	
 	add_theme_support( 'automatic-feed-links' );
 
+<<<<<<< HEAD
 	/**
 	 * Custom Theme Options
 	 */
@@ -38,6 +39,8 @@ function the_bootstrap_setup() {
 	 */
 	require_once ( get_template_directory() . '/inc/custom-header.php' );
 	
+=======
+>>>>>>> refs/remotes/origin/master
 	add_theme_support( 'post-formats', array(
 		'aside',
 		'chat',
@@ -51,7 +54,29 @@ function the_bootstrap_setup() {
 
 	add_custom_background();
 	
-	register_nav_menu( 'primary', __( 'Main Navigation', 'the-bootstrap' ) );
+	/**
+	 * Custom template tags for this theme.
+	 */
+	require_once ( get_template_directory() . '/inc/template-tags.php' );
+	
+	/**
+	 * Implement the Custom Header feature
+	 */
+	require_once ( get_template_directory() . '/inc/custom-header.php' );
+	
+	/**
+	 * Including three menu (header-menu, primary and footer-menu).
+	 * Primary is wrapping in a navbar containing div (wich support responsive variation)
+	 * Header_menu and Footer-menu are inside pills dropdown menu
+	 * 
+	 * @since	1.2.2 - 07.04.2012
+	 * @see		http://codex.wordpress.org/Function_Reference/register_nav_menus
+	 */
+	register_nav_menus( array(
+		'primary'		=>	__( 'Main Navigation', 'the-bootstrap' ),
+		'header-menu'  	=>	__( 'Header Menu', 'the-bootstrap' ),
+		'footer-menu' 	=>	__( 'Footer Menu', 'the-bootstrap' )
+	) );
 	
 } // the_bootstrap_setup
 endif;
@@ -79,6 +104,7 @@ function the_bootstrap_widgets_init() {
 	
 	register_sidebar( array(
 		'name'			=>	__( 'Image Sidebar', 'the-bootstrap' ),
+		'description'	=>	__( 'Shown on image attchment pages.', 'the-bootstrap' ),
 		'id'			=>	'image',
 		'before_widget'	=>	'<aside id="%1$s" class="widget well %2$s">',
 		'after_widget'	=>	"</aside>",
@@ -103,7 +129,7 @@ add_action( 'widgets_init', 'the_bootstrap_widgets_init' );
 function the_bootstrap_register_scripts_styles() {
 
 	if ( ! is_admin() ) {
-		$theme_data = get_theme_data( get_template_directory() . '/style.css' );
+		$theme_version = _the_bootstrap_version();
 		$suffix = ( defined('SCRIPT_DEBUG') AND SCRIPT_DEBUG ) ? '' : '.min';
 			
 		/**
@@ -120,8 +146,8 @@ function the_bootstrap_register_scripts_styles() {
 		wp_register_script(
 			'the-bootstrap',
 			get_template_directory_uri() . "/js/the-bootstrap{$suffix}.js",
-			array('jquery'),
-			$theme_data['Version'],
+			array('tw-bootstrap'),
+			$theme_version,
 			true
 		);
 				
@@ -139,7 +165,7 @@ function the_bootstrap_register_scripts_styles() {
 			'the-bootstrap',
 			get_template_directory_uri() . "/style{$suffix}.css",
 			array('tw-bootstrap'),
-			$theme_data['Version']
+			$theme_version
 		);
 	}
 }
@@ -155,7 +181,6 @@ add_action( 'init', 'the_bootstrap_register_scripts_styles' );
  * @return	void
  */
 function the_bootstrap_print_scripts() {
-	wp_enqueue_script( 'tw-bootstrap' );
 	wp_enqueue_script( 'the-bootstrap' );
 
 	if ( is_singular() AND comments_open() AND get_option( 'thread_comments' ) ) {
@@ -182,148 +207,23 @@ function the_bootstrap_print_styles() {
 add_action( 'wp_enqueue_scripts', 'the_bootstrap_print_styles' );
 
 
-/**
- * Display navigation to next/previous pages when applicable
- *
- * To be honest - I'm pretty proud of this function. Through a lot of trial and
- * error, I was able to user a core WordPress function (paginate_links()) and
- * adjust it in a way, that the end result is a legitimate pagination.
- * A pagination many developers buy (code) expensively with Plugins like
- * WP Pagenavi. No need! WordPress has it all!
- *
- * @author	Konstantin Obenland
- * @since	1.0.0 - 05.02.2012
- *
- * @return	void
- */
-function the_bootstrap_content_nav() {
-	global $wp_query;
-
-	$paged			=	( get_query_var( 'paged' ) ) ? intval( get_query_var( 'paged' ) ) : 1;
-	$pagenum_link	=	get_pagenum_link();
-	$url_parts		=	parse_url( $pagenum_link );
-	$format			=	( get_option('permalink_structure') ) ? user_trailingslashit('page/%#%', 'paged') : '?paged=%#%';
-	
-	if ( isset($url_parts['query']) ) {
-		$pagenum_link	=	"{$url_parts['scheme']}://{$url_parts['host']}{$url_parts['path']}%_%?{$url_parts['query']}";
-	} else {
-		$pagenum_link	.=	'%_%';
-	}
-	
-	$links	=	paginate_links( array(
-		'base'		=>	$pagenum_link,
-		'format'	=>	$format,
-		'total'		=>	$wp_query->max_num_pages,
-		'current'	=>	$paged,
-		'mid_size'	=>	3,
-		'type'		=>	'list'
-	) );
-	
-	if ( $links ) {
-		echo "<nav class=\"pagination pagination-centered\">{$links}</nav>";
-	}
-}
-
-if ( ! function_exists( 'the_bootstrap_posted_on' ) ) :
+if ( ! function_exists( 'the_bootstrap_credits' ) ) :
 /**
  * Prints HTML with meta information for the current post-date/time and author,
  * comment and edit link
  *
  * @author	Konstantin Obenland
- * @since	1.0.0 - 05.02.2012
+ * @since	1.2.2 - 07.04.2012
  *
- * @param	array	$args
- *
- * @return	void|string
+ * @return	void
  */
-function the_bootstrap_posted_on() {
-	printf( __( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s" pubdate>%4$s</time></a><span class="by-author"> <span class="sep"> by </span> <span class="author vcard"><a class="url fn n" href="%5$s" title="%6$s" rel="author">%7$s</a></span></span>', 'the-bootstrap' ),
-		esc_url( get_permalink() ),
-		esc_attr( get_the_time() ),
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-		esc_attr( sprintf( __( 'View all posts by %s', 'the-bootstrap' ), get_the_author() ) ),
-		get_the_author()
+function the_bootstrap_credits() {
+	printf(
+		__('&copy; %1$s <a href="%2$s">%3$s</a>, all rights reserved.', 'the-bootstrap'),
+		date( 'Y' ),
+		home_url( '/' ),
+		get_bloginfo( 'name' )
 	);
-	if ( comments_open() AND ! post_password_required() ) { ?>
-		<span class="sep"> | </span>
-		<span class="comments-link">
-			<?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a reply', 'the-bootstrap' ) . '</span>', __( '<strong>1</strong> Reply', 'the-bootstrap' ), __( '<strong>%</strong> Replies', 'the-bootstrap' ) ); ?>
-		</span>
-		<?php
-	}
-	edit_post_link( __( 'Edit', 'the-bootstrap' ), '<span class="sep">&nbsp;</span><span class="edit-link label">', '</span>' );
-}
-endif;
-
-
-if ( ! function_exists( 'the_bootstrap_link_pages' ) ) :
-/**
- * Displays page links for paginated posts
- *
- * It's basically the wp_link_pages() function, altered to fit to the Bootstrap
- * markup needs for paginations (unordered list).
- * @see		wp_link_pages()
- *
- * @author	Konstantin Obenland
- * @since	1.1.0 - 09.03.2012
- *
- * @param	array	$args
- *
- * @return	void|string
- */
-function the_bootstrap_link_pages( $args = array() ) {
-	wp_link_pages( array( 'echo' => 0 ));
-	$defaults = array(
-		'next_or_number'	=> 'number',
-		'nextpagelink'		=> __('Next page', 'the-bootstrap'),
-		'previouspagelink'	=> __('Previous page', 'the-bootstrap'),
-		'pagelink'			=> '%',
-		'echo'				=> true
-	);
-
-	$r = wp_parse_args( $args, $defaults );
-	$r = apply_filters( 'the_bootstrap_link_pages_args', $r );
-	extract( $r, EXTR_SKIP );
-
-	global $page, $numpages, $multipage, $more, $pagenow;
-
-	$output = '';
-	if ( $multipage ) {
-		if ( 'number' == $next_or_number ) {
-			$output .= '<nav class="pagination clear"><ul><li><span class="dots">' . __('Pages:', 'the-bootstrap') . '</span></li>';
-			for ( $i = 1; $i < ($numpages + 1); $i++ ) {
-				$j = str_replace( '%', $i, $pagelink );
-				if ( ($i != $page) || ((!$more) && ($page!=1)) ) {
-					$output .= '<li>' . _wp_link_page($i) . $j . '</a></li>';
-				}
-				if ($i == $page) {
-					$output .= '<li class="current"><span>' . $j . '</span></li>';
-				}
-				
-			}
-			$output .= '</ul></nav>';
-		} else {
-			if ( $more ) {
-				$output .= '<nav class="pagination clear"><ul><li><span class="dots">' . __('Pages:', 'the-bootstrap') . '</span></li>';
-				$i = $page - 1;
-				if ( $i && $more ) {
-					$output .= '<li>' . _wp_link_page( $i ) . $previouspagelink. '</a></li>';
-				}
-				$i = $page + 1;
-				if ( $i <= $numpages && $more ) {
-					$output .= '<li>' . _wp_link_page( $i ) . $nextpagelink. '</a></li>';
-				}
-				$output .= '</ul></nav>';
-			}
-		}
-	}
-
-	if ( $echo )
-		echo $output;
-
-	return $output;
 }
 endif;
 
@@ -873,6 +773,28 @@ function the_bootstrap_the_password_form( $form ) {
 }
 add_filter( 'the_password_form', 'the_bootstrap_the_password_form' );
 
+
+/**
+ * Returns the Theme version string
+ *
+ * @author	Konstantin Obenland
+ * @since	1.2.4 - 07.04.2012
+ * @access	private
+ *
+ * @return	string	The Bootstrap version
+ */
+function _the_bootstrap_version() {
+	
+	if ( version_compare( get_bloginfo( 'version' ), '3.3.1', '>' ) ) {
+		$theme_version = wp_get_theme()->get( 'Version' );
+	}
+	else {
+		$theme_data = get_theme_data( get_template_directory() . '/style.css' );
+		$theme_version = $theme_data['Version'];
+	}
+	
+	return $theme_version;
+}
 
 /* End of file functions.php */
 /* Location: ./wp-content/themes/the-bootstrap/functions.php */
